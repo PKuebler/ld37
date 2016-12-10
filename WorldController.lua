84a2410490acb36f75466e642526a2a453c84b9a
Philipp Kuebler <info@pkuebler.de>
2016-12-10 13:26:17 +0100

    add factory to worldcontroller

--- WorldController.lua
+++ WorldController.lua
@@ -2,7 +2,8 @@ require("TileMap")
 
 function WorldController()
 	local self = {
-		tilemap = TileMap(20,20,16,16)
+		tilemap = TileMap(20,20,16,16),
+		factory = require("logic.Factory")
 	}
 
 	function self.load()
---
+++ logic/Factory.lua
@@ -0,0 +1,23 @@
+return {
+	easycash = {
+		name = "easycash",
+		usetime = 5, -- sekunden
+		buildcosts = 20, -- baukosten
+		money = 5, -- positive zahl adiert charakter geld, negative zieht ab und addiert zu konto
+		sprite = 2
+	},
+	bench = {
+		name = "bench",
+		usetime = 5,
+		buildcosts = 10,
+		money = 0,
+		sprite = 1
+	},
+	oneArmedBandit = {
+		name = "oneArmedBandit",
+		usetime = 10,
+		buildcosts = 20,
+		money = -10,
+		sprite = 3
+	}
+}
