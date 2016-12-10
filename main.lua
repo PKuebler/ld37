7881c05c5b79e044e3b3ae077c4b9218e32a7c0f
Mezzenstein <mail@danielmoeck.de>
2016-12-10 05:06:55 +0100

    ain und gitignore

---
+++ main.lua
@@ -0,0 +1,22 @@
+
+
+-- instant console
+io.stdout:setvbuf('no')
+
+--local stages = StageManager()
+
+function love.load()
+	--stages.add("menu",MenuStage)
+	--stages.add("game",GameStage)
+
+	--stages.toggle("game")
+	print("alive")
+end
+
+function love.update(dt)
+	--stages.update(dt)
+end
+
+function love.draw()
+	--stages.draw()
+end
